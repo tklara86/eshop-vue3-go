73cae3f4fd7ce50b7fceb657333406ce46245dae
Tomasz Klara <tomaszklara@gmail.com>
2022-12-04 12:03:06 +0000

    updates

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
-
 import App from './App.vue'
 import router from './router'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 const app = createApp(App)
 
