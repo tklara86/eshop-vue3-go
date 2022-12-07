7d5d92ec43e2faf216729e330bcf73391addcc38
Tomasz Klara <tomaszklara@gmail.com>
2022-12-07 21:48:28 +0000

    added gin

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	time := time.Now()
-
-	fmt.Println(time)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":9090")
 }
